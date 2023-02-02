f0a0aa46da40477c8e9a33b1455930782ef30b14
José Luis Sepúlveda Tarazona <josexbr@gmail.com>
2023-02-02 15:06:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 
-streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Breakfasst Menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
